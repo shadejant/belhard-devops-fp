3f69a3f9abc60c770cc06acac8d34b882456bd45
Aleksandr Blogushko <106614275+shadejant@users.noreply.github.com>
2022-07-18 16:39:19 +0300

    Delete 01.vm directory

--- 01.vm/03-vars.tfvars
+++
@@ -1,9 +0,0 @@
-dc = "NCA-Blade-DC"
-cluster = "Blade-HA-Cluster"
-host
-vswitch
-bh-k3s["hostname"]
-bh-k3s["vcpu"]
-bh-k3s["ram"]
-bh-k3s["disk_datastore"]
-bh-k3s["disk_size"]
--- 01.vm/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "vsphere" {
-  user                 = "VSPHERE.LOCAL\\bh-devops"
-  password             = "12345678"
-  vsphere_server       = "ncavcenter"
-  allow_unverified_ssl = true
-}
